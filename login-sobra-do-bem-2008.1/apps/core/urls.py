4b95ab809ce8feebd6f0aca8531dbe10647fdb6d
e.mendes <pemendes11@gmail.com>
2025-09-15 20:46:59 -0300

    tela de login inicial

--- login-sobra-do-bem-2008.1/apps/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),      # Aqui usamos index
-    path('login/', views.login_view, name='login'),
-    path('cadastro/', views.cadastro, name='cadastro'),
-]
--- login-sobra-do-bem-2008.1/sobradobem/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apps.core.urls')),        # Rotas do seu app core
-    path('accounts/', include('allauth.urls')), # Rotas de login social (Google, etc)
-]
